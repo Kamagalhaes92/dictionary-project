d60cab5d4898ea0e25f36399b4ba2e1a3e449080
Karine Magalhaes Heidgerken <karinemagalhaes92@gmail.com>
2024-06-14 22:22:54 -0300

    Rendered the meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null);
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response);
+        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
 
@@ -22,10 +25,13 @@ export default function Dictionary() {
         setKeyword(event.target.value);
     }
 
-    return <div className="Dictionary">
+    return (<div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} autoFocus={true} />
         </form>
+        
+        <Results results={results} />
     </div>
+    );
 
 }
