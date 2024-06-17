929a5a23e2ad63e4ad5869747fab6d29cf61946c
Karine Magalhaes Heidgerken <karinemagalhaes92@gmail.com>
2024-06-17 11:05:15 -0300

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,25 +1,29 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+
 
 export default function Meaning (props) {
-    console.log(props.meaning);
+    
     return (
         <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index) 
-        {
+          <h3>{props.meaning.partOfSpeech}</h3>
+          {props.meaning.definitions.map(function (definition, index) {
             return (
-                <div key={index}>
-                     <p>
-                        {definition.definition}
-                        <br/>
-                        <em>{definition.example}</em>
-                     </p>
-                </div>
+              <div key={index}>
+                <p>
+                  <strong>Definition:</strong> {definition.definition}
+                  <br />
+                  {definition.example && ( // Check if example exists
+                    <>
+                      <strong>Example:</strong> <em>{definition.example}</em>
+                      <br />
+                      <Synonyms synonyms={definition.synonyms} />
+                    </>
+                  )}
+                </p>
+              </div>
             );
-           
-        })}
-        
+          })}
         </div>
-        );
-    
-}
+      );
+    }
--- src/Results.js
+++ src/Results.js
@@ -1,28 +1,38 @@
 import React from "react";
 import Meaning from "./Meaning";
 
+import './Results.css';
+import Typewriter from "typewriter-effect";
+
 
 export default function Results(props){
     if (props.results) {
         return (
-            <div className="Results">
-                <h2>{props.results.word}</h2>
-                {props.results.meanings.map(function(meaning, index){
-                    return (
-                        <div key={index}>
-                            <Meaning meaning={meaning} />
-
-                        </div>
-                    )
-                })}
-                
-          
-
-            </div>
+          <div className="Results">
+            <h2 className="Typewriter">
+              <Typewriter
+                options={{
+                  strings: [props.results.word],
+                  autoStart: true,
+                  delay: 85,
+                  deleteSpeed: 1000000,
+                  loop: false,
+                  cursor: ""
+                  
+                }}
+              />
+            </h2>
+            {props.results.meanings.map(function (meaning, index) {
+              return (
+                <div key={index}>
+                  <Meaning meaning={meaning} />
+                </div>
+              );
+            })}
+          </div>
         );
-    } else {
+      } else {
         return null;
+      }
     }
-    
-}
 
