568fbafdd9394c87172ea665059cb87fe3f8140c
Karine Magalhaes Heidgerken <karinemagalhaes92@gmail.com>
2024-06-20 15:28:47 -0300

    Added spline animation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import './App.css';
 import Dictionary from './Dictionary';
 import WordOfTheDay from './WordOfTheDay';
-
+import SplineViewer from './SplineViewer';
 
 export default function App() {
-    const [showWordOfTheDay, setShowWordOfTheDay] = useState(true);
+  const [showWordOfTheDay, setShowWordOfTheDay] = useState(true);
 
-    const handleSearch = () => {
-        setShowWordOfTheDay(false);
-    };
+  const handleSearch = () => {
+    setShowWordOfTheDay(false);
+  };
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                WORDWAVE
-            </header>
-            <div className="main-content">
-                <div className="vertical-menu">
-                    <div className="vertical-line-container">
-                        <div className="vertical-line"></div>
-                        <div className="menu-item">
-                            <a href="#main">MAIN</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='container'>
-                    <div className="headline-container">
-                        <h2 className='headline'>Revolutionize <br/> Your Vocabulary <br/>with WordWave</h2>
-                        
-                    </div>
-                    <main>
-                        
-                        <Dictionary onSearch={handleSearch} />
-                        {showWordOfTheDay && <WordOfTheDay />}
-                    </main>
-                    <footer className='text-center'>
-                        Coded by Karine Magalhaes.
-                    </footer>
-                </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        WORDWAVE
+      </header>
+      <div className="main-content">
+        <div className="vertical-menu">
+          <div className="vertical-line-container">
+            <div className="vertical-line"></div>
+            <div className="menu-item">
+              <a href="#main">MAIN</a>
             </div>
+          </div>
         </div>
-    );
+        <div className="container">
+          <div className="headline-container">
+            <h2 className='headline'>Revolutionize <br /> Your Vocabulary <br />with WordWave</h2>
+            <SplineViewer />
+          </div>
+          <main>
+            <Dictionary onSearch={handleSearch} />
+            {showWordOfTheDay && <WordOfTheDay />}
+          </main>
+          <footer className='text-center'>
+            Coded by Karine Magalhaes.
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
 }
